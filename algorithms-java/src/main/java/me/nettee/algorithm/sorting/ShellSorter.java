57bf850667f131d9c76b108bb96f5cbcf4a01f98
William <nettee.liu@gmail.com>
2018-04-13 15:12:55 +0800

    shell sort improved

--- algorithms-java/src/main/java/me/nettee/algorithm/sorting/ShellSorter.java
+++ algorithms-java/src/main/java/me/nettee/algorithm/sorting/ShellSorter.java
@@ -5,19 +5,29 @@ public class ShellSorter extends Sorter {
     @Override
     void sort(Comparable[] a) {
         int N = a.length;
-        // Init h to a largest shell number (no larger than N / 3)
+
+        // Init h to the largest gap number (no larger than N / 3)
+        // Use gap sequences 1, 4, 13, 40, 121, 364, 1093, ...
+        // There are many other gap sequences
         int h = 1;
         while (h < N / 3) {
             h = 3 * h + 1;
         }
 
+        // After each iteration, a is h-sorted, i.e., every h-th element
+        // gives a sorted list.
+        // After the last iteration (h = 1), the list will be entirely sorted.
         while (h >= 1) {
-            // Local insertion sort
+            // Use insertion sort to h-sort the list.
             for (int i = h; i < N; i++) {
                 // Insert a[i] into a[i-h], a[i-2h], a[i-3h] ...
-                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h) {
-                    exchange(a, j, j-h);
+                Comparable t = a[i];
+                int j = i;
+                while (j >= h && less(t, a[j-h])) {
+                    a[j] = a[j-h];
+                    j -= h;
                 }
+                a[j] = t;
             }
             h /= 3;
         }
