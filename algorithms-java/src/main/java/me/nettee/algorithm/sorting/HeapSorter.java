579dc5597f1ba9586ad49385ee8a07616e51fee5
William <nettee.liu@gmail.com>
2018-04-16 17:55:56 +0800

    leave heap interface for data structure impl

--- algorithms-java/src/main/java/me/nettee/algorithm/sorting/HeapSorter.java
+++ algorithms-java/src/main/java/me/nettee/algorithm/sorting/HeapSorter.java
@@ -4,11 +4,40 @@ import java.util.PriorityQueue;
 
 public class HeapSorter extends Sorter {
 
+    public interface Heap<E extends Comparable> {
+        void add(E e);
+        E remove();
+    }
+
+    static class DefaultHeap implements Heap<Comparable> {
+
+        private final PriorityQueue<Comparable> heap = new PriorityQueue<>();
+
+        @Override
+        public void add(Comparable e) {
+            heap.add(e);
+        }
+
+        @Override
+        public Comparable remove() {
+            return heap.remove();
+        }
+    }
+
+    private Heap<Comparable> heap;
+
+    public HeapSorter() {
+        setHeap(new DefaultHeap());
+    }
+
+    void setHeap(Heap<Comparable> heap) {
+        this.heap = heap;
+    }
+
     @Override
     void sort(Comparable[] a) {
         int N = a.length;
 
-        PriorityQueue<Comparable> heap = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             heap.add(a[i]);
         }
